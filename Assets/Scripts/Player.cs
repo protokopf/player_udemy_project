f055e5b58155b4163bf6f25b4ae996649a17a11a
Konstantin Kovalenko <protokopf@gmail.com>
2018-08-11 00:21:51 +0300

    - Improved jumping a little bit.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.Abstract;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -6,9 +7,7 @@ namespace Assets.Scripts
     public class Player : MonoBehaviour
     {
         private bool _wasGrounded = true;
-
-        [SerializeField]
-        private bool _isJumping;
+        private bool _isJumping = false;
 
         public AbstractMovementBehaviour MovementBehaviour;
         public AbstractAnimationBehaviour AnimationBehaviour;
@@ -17,62 +16,52 @@ namespace Assets.Scripts
 
         public float JumpAcceleration;
 
+        public float JumpOffsetSeconds;
+
         public float Speed = 1.0f;
 
         public float RaycastDistance;
 
         public LayerMask LayerMask;
-
-
-        // Use this for initialization
-        public void Start ()
-        {
-        }
 	
         // Update is called once per frame
         public void Update ()
         {
             float horizontalMovement = MovementBehaviour.GetHorizontalMovement();
-            float verticalMovement = RidigBody.velocity.y;
             Debug.DrawRay(transform.position, Vector2.down * RaycastDistance, Color.green);
 
             bool isGrounded = IsGrounded();
 
-            if(_wasGrounded && !isGrounded)
-            {
-                _isJumping = true;
-            }
-            else if(!_wasGrounded && isGrounded)
+            if(!_wasGrounded && isGrounded)
             {
                 _isJumping = false;
             }
-
-            if(_isJumping)
-            {
-                AnimationBehaviour.Jump();
-            }
-            else
-            {
-                AnimationBehaviour.HorizontalMove(horizontalMovement);
-            }
-
             _wasGrounded = isGrounded;
 
-
-            if (MovementBehaviour.GetHasJumped() && isGrounded)
+            if (!_isJumping)
             {
-                verticalMovement += JumpAcceleration;
+                AnimationBehaviour.HorizontalMove(horizontalMovement);
+                if (MovementBehaviour.GetHasJumped() && isGrounded)
+                {
+                    _isJumping = true;
+                    AnimationBehaviour.Jump();
+                    StartCoroutine(JumpCoroutine());
+                }
             }
 
-            RidigBody.velocity = new Vector2(horizontalMovement * Speed, verticalMovement);
-
-            
+            RidigBody.velocity = new Vector2(horizontalMovement * Speed, RidigBody.velocity.y);         
         }
 
         private bool IsGrounded()
         {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, RaycastDistance, LayerMask);
             return hit.collider != null;
+        }
+
+        private IEnumerator JumpCoroutine()
+        {
+            yield return new WaitForSeconds(JumpOffsetSeconds);
+            RidigBody.velocity = new Vector2(RidigBody.velocity.x, RidigBody.velocity.y + JumpAcceleration);
         }
     }
 }
